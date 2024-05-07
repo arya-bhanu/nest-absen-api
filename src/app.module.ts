b36655f2ee72bc763c2377f4c7d792a13a5a50cc
arya-bhanu <aryacreative.dev@gmail.com>
2024-05-07 23:36:11 +0700

    init project port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+@Global()
 @Module({
   imports: [],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  dotenv.config();
+  const port = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  Logger.log(`Running at port [${port}]`);
+  await app.listen(port);
 }
 
 bootstrap();
